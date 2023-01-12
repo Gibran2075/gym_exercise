9ccccbbacdca33d01629d0ac81e8618fcb794172
gibran <m.giibraan@gmail.com>
2023-01-12 13:38:59 +0700

    commit

---
+++ src/components/Exercises.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from "react";
+import Pagination from "@mui/material/Pagination";
+import { Box, Stack, Typography } from "@mui/material";
+
+import { exerciseOptions, fetchData } from "../utils/fetchData";
+
+const Exercises = ({ Exercises, setExercises, bodyPart }) => {
+  return <div>Exercises</div>;
+};
+
+export default Exercises;
