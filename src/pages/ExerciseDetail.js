e038576feb72918081ff3192d29fa4e6e6adafad
gibran <m.giibraan@gmail.com>
2023-01-17 22:32:43 +0700

    youtube url

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -8,12 +8,16 @@ import ExerciseVideos from "../components/ExerciseVideos";
 import SimilarExercises from "../components/SimilarExercises";
 
 const ExerciseDetail = () => {
-  const [ExerciseDetail, setExerciseDetail] = useState({});
+  const [exerciseDetail, setExerciseDetail] = useState({});
   const { id } = useParams;
 
   useEffect(() => {
     const fetchExercisesData = async () => {
       const exerciseDbUrl = "https://exercisedb.p.rapidapi.com";
+      const youtubeSearchUrl = "https://youtube-search-and-download.p.rapidapi.com/channel/about";
+
+      const exerciseDetailData = await fetchData(`@{exerciseDbUrl}/exercises/${id}`, exerciseOptions);
+      setExerciseDetail(exerciseDetailData);
     };
     fetchExercisesData;
   }, [id]);
